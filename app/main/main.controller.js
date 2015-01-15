28d499d8056ac4841730eae31358d5d479abfb30
meredithmmyers <myers.mm89@gmail.com>
2015-01-15 14:49:30 -0500

    Now with comments

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -2,13 +2,22 @@
 
 angular.module('ratmap')
   .controller('MainCtrl', function ($scope, $http, $timeout) {
+      /* Default the heatmap as showing/not showing */
       $scope.hShow = true;
+      /* Default the marker layer as showing/not showing */
       $scope.mShow = true;
+
+      // ------------------------------------------------------------------------------
+      // ----------------------------- Data Request -----------------------------------
+      // ------------------------------------------------------------------------------
+
       $http.get('http://data.cityofnewyork.us/resource/3q43-55fe?$limit=10000', {
         cache: true
       }).
         success(function(data, status, headers, config) {
+          /* Get the minimum date for display */
           data.minDate = formatDate(new Date(Math.min.apply(null,_.map(_.pluck(data, 'created_date'), function(d) { return new Date(d);}))));
+          /* Set the data model */
           $scope.data = data;
           $scope.loaded = true;
         }).
@@ -16,7 +25,16 @@ angular.module('ratmap')
           console.log(status);
         });
 
+        // ------------------------------------------------------------------------------
+        // ---------------------------------- Events ------------------------------------
+        // ------------------------------------------------------------------------------
+
+        /* 
+        * Event listener for a marker click 
+        * @param m - the marker 
+        */
         $scope.$on('marker-click', function(ev, m) {
+          /* Format display attributes */
           var address = makeAddress(m);
           var date = formatDate(new Date(m.created_date.split("T")[0]));
           var city = formatCity(m);
@@ -27,16 +45,24 @@ angular.module('ratmap')
             'address': address,
             'city': city
           };
+          /* Set the model */
           $scope.selected = item;
           $timeout(function() {
             $scope.$apply();
           },1);
         });
 
+        // ------------------------------------------------------------------------------
+        // ------------------------ Display Formatting ----------------------------------
+        // ------------------------------------------------------------------------------
+
+        /* Broadcasts an event to tell the heatmap and marker layer to show or not show 
+        * @param type = markers || heatmap */
         $scope.show = function(type) {
           $scope.$broadcast('toggle-display', type);
         }
 
+        /* Creates the address based on available fields */
         function makeAddress(m) {
           if (m.cross_street_1 && m.cross_street_2)
             return setCases(m.cross_street_1) + " & " + setCases(m.cross_street_2);
@@ -48,14 +74,17 @@ angular.module('ratmap')
             console.log(m)
         }
 
+        /* Sets the first letters of the city's name to uppercase */
         function formatCity(m) {
           return setCases(m.city);
         }
 
+        /* Format the marker's date for display */
         function formatDate(d) {
           return (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
         }
 
+        /* Sets the first letter of each word in a space delimited string to caps and the other letters to lower */
         function setCases(m) {
           var formatted = "";
           _.each(m.split(" "), function(d) {
@@ -63,5 +92,4 @@ angular.module('ratmap')
           })
           return formatted;
         }
-
   });
--- components/map/map.directive.js
+++ components/map/map.directive.js
@@ -7,60 +7,77 @@ directive('map', ['$window',
                 data: "="
             },
             link: function(scope, element, attrs) {
-                var map, heat, markers, heatmapShowing = true, markersShowing = true;
+                var map, heat, markers, heatmapShowing = true,
+                    markersShowing = true;
 
+                // Set the map height equivalent to the window height
                 $('#map').height($(window).height());
 
+                // Watch for model data changes
                 scope.$watch('data', function(newVal) {
-                  if (newVal)
-                    draw(newVal);
+                    if (newVal)
+                        draw(newVal);
                 });
 
+                // Watch for display toggle between heatmap / marker layer
                 scope.$on('toggle-display', function(ev, type) {
-                  if (map && heat && markers) {
-                    switch(type) {
-                      case "heatmap":
-                        if (heatmapShowing)
-                          map.removeLayer(heat);
-                        else
-                          map.addLayer(heat);
-                        heatmapShowing = !heatmapShowing;
-                        return;
-                      case "markers":
-                        if (markersShowing)
-                          map.removeLayer(markers);
-                        else
-                          map.addLayer(markers);
-                        markersShowing = !markersShowing;
-                        return;
+                    if (map && heat && markers) {
+                        switch (type) {
+                            case "heatmap":
+                                if (heatmapShowing)
+                                    map.removeLayer(heat);
+                                else
+                                    map.addLayer(heat);
+                                heatmapShowing = !heatmapShowing;
+                                return;
+                            case "markers":
+                                if (markersShowing)
+                                    map.removeLayer(markers);
+                                else
+                                    map.addLayer(markers);
+                                markersShowing = !markersShowing;
+                                return;
+                        }
                     }
-                  }
                 })
 
+                // Emit a marker-click event to the controller on marker click
+                function markerClick(d) {
+                    scope.$emit('marker-click', d.layer.options.item);
+                }
+                
+                // Update the map height on page resize
                 $window.onresize = function(event) {
                     $('#map').height($(window).height());
                 }
 
-                map = L.map('map',{
-                  minZoom: 4,
-                  maxZoom: 17,
-                  zoomControl: false
+                // Create the map
+                map = L.map('map', {
+                    minZoom: 4,
+                    maxZoom: 17,
+                    zoomControl: false
                 }).setView([40.712784, -74.005941], 12);
 
-                new L.Control.Zoom({ position: 'topright' }).addTo(map);
+                // Add the zoom control
+                new L.Control.Zoom({
+                    position: 'topright'
+                }).addTo(map);
 
-                // alternative themes: 'terrain' and 'watercolor'.
+                // Add the tile layer. Alternative themes: 'terrain' and 'watercolor'.
                 var canvas = L.tileLayer.provider('Esri.WorldGrayCanvas');
 
+                // Add the tile layer
                 map.addLayer(canvas);
 
-                // create the geocoding control and add it to the map
-                var searchControl = new L.esri.Geocoding.Controls.Geosearch({ position: 'topright' }).addTo(map);
+                // Create the geocoding control and add it to the map
+                var searchControl = new L.esri.Geocoding.Controls.Geosearch({
+                    position: 'topright'
+                }).addTo(map);
 
-                // create an empty layer group to store the results and add it to the map
+                // Create an empty layer group to store the results and add it to the map
                 var results = new L.LayerGroup().addTo(map);
 
-                // listen for the results event and add every result to the map
+                // Listen for the results event and add every result to the map
                 searchControl.on("results", function(data) {
                     results.clearLayers();
                     for (var i = data.results.length - 1; i >= 0; i--) {
@@ -68,48 +85,55 @@ directive('map', ['$window',
                     };
                 });
 
-                function markerClick(d) {
-                    scope.$emit('marker-click', d.layer.options.item);
-                }
+                // Create custom marker icon
                 var customMarker = L.Marker.extend({
-                   options: {
-                      item: undefined
-                   }
+                    options: {
+                        item: undefined
+                    }
                 });
-                var myIcon = L.icon({ 
-                    iconUrl: 'assets/images/marker.svg', 
+                var myIcon = L.icon({
+                    iconUrl: 'assets/images/marker.svg',
                     shadowSize: [0, 0],
                     iconSize: [38, 95]
                 });
+
+                // Draw the map
                 function draw(data) {
                     var latLngs = [];
+                    // Create the marker custer layer
                     markers = new L.MarkerClusterGroup();
+                    // For each rat report!
                     _.each(data, function(d) {
-                      if (d.latitude && d.longitude) {
-                        var m = new customMarker([parseFloat(d.latitude), parseFloat(d.longitude)], {
-                            item: d,
-                            icon: myIcon
-                        });
-                        markers.addLayer(m);
-                        latLngs.push([parseFloat(d.latitude), parseFloat(d.longitude)]);
-                      }
+                        if (d.latitude && d.longitude) {
+                            // Create a custom marker
+                            var m = new customMarker([parseFloat(d.latitude), parseFloat(d.longitude)], {
+                                item: d,
+                                icon: myIcon
+                            });
+                            markers.addLayer(m);
+                            // Create the array of lat lngs for the heatlayer
+                            latLngs.push([parseFloat(d.latitude), parseFloat(d.longitude)]);
+                        }
                     });
+                    // Add click listener
                     markers.on('click', markerClick);
 
-                 if (markersShowing)
-                  map.addLayer(markers);
-                
-                 heat = L.heatLayer(latLngs, {
-                     radius: 16,
-                     gradient: {
-                         0.4: '#1b5479',
-                         0.65: '#d4e6f1',
-                         1: '#D35400'
-                     }
-                 });
+                    // Add the layer
+                    if (markersShowing)
+                        map.addLayer(markers);
 
-                 if (heatmapShowing)
-                  map.addLayer(heat);
+                    // Create the heatmap
+                    heat = L.heatLayer(latLngs, {
+                        radius: 16,
+                        gradient: {
+                            0.4: '#1b5479',
+                            0.65: '#d4e6f1',
+                            1: '#D35400'
+                        }
+                    });
+
+                    if (heatmapShowing)
+                        map.addLayer(heat);
 
                 }
             }
